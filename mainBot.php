5abe1ae22e93d4d67c6e2275dc85d271c1cfe3ff
Smoker-Ldr <donhbeattie@yahoo.com>
2016-12-06 15:33:19 -0500

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token = "e7033c444802286552fa35ec70";
 
 include('./index.php');
 
